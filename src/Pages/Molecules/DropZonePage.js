def138ca23590940176bc23b6ad3758295065240
mutiadianita <mutiaayu.dianita@gmail.com>
2020-08-03 07:33:37 +0700

    update

--- src/Pages/Molecules/DropZonePage.js
+++ src/Pages/Molecules/DropZonePage.js
@@ -38,21 +38,33 @@ class DropZonePage extends React.Component {
         <DropZone
           icon
           stylesdropzone="dropzone--small"/>
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--small"/&gt;
+        </code>
         <br />
         <h2 className="subtitle">Default</h2>
         <DropZone
           icon/>
+        <code>
+        &lt;DropZone icon/&gt;
+        </code>
         <br />
         <h2 className="subtitle">Large</h2>
         <DropZone
           icon
           stylesdropzone="dropzone--large"/>
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--large"/&gt;
+        </code>
         <br />
         <h2 className="subtitle">XLarge</h2>
         <DropZone
           icon
           stylesdropzone="dropzone--xlarge"/>
-          <br />
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--xlarge"/&gt;
+        </code>
+        <br/>
         <h1>Type</h1>
         <br />
         <h2 className="subtitle">Basic</h2>
@@ -61,21 +73,33 @@ class DropZonePage extends React.Component {
           icon
           stylesdropzone="dropzone--small"
           note />
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--small" note/&gt;
+        </code>
         <br />
         <DropZone
           icon
           stylesdropzone="dropzone--small"/>
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--small"/&gt;
+        </code>
         <br />
         <h2 className="subtitle">Basic Button</h2>
         <DropZone
           default
           note />
+        <code>
+        &lt;DropZone default note/&gt;
+        </code>
         <br />
         <h2 className="subtitle">Multiple Upload</h2>
         <DropZone
           icon
           note
           stylesdropzone="dropzone--small multimedia"/>
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--small multimedia"/&gt;
+        </code>
         <br />
         <h1>State</h1>
         <br />
@@ -84,6 +108,10 @@ class DropZonePage extends React.Component {
         <DropZone
           icon
           stylesdropzone="dropzone--small"/>
+        
+        <code>
+        &lt;DropZone icon stylesdropzone="dropzone--small"/&gt;
+        </code>
       </div>
     )
   }
